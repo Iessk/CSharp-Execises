5f9f74f5cb4823c5bd930ca5e76015c7589c0e19
Ali Almeshal <rrrxhero99@gmail.com>
2026-09-16 12:36:34 +0300

    Add files via upload

---
+++ Dynamic.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Main
+{
+    internal class Dynamic
+    {
+
+        static void Main(string[] args)
+        {
+
+            dynamic MyDynamicVar = 100;
+            Console.WriteLine("Value : {0} , Type : {1} ", MyDynamicVar, MyDynamicVar.GetType());
+
+            MyDynamicVar = "Hello, World!";
+            Console.WriteLine("Value: {0} , Type : {1} ", MyDynamicVar, MyDynamicVar.GetType());
+
+            MyDynamicVar = false;
+            Console.WriteLine("Value : {0} , Type : {1} ", MyDynamicVar, MyDynamicVar.GetType());
+
+            MyDynamicVar = DateTime.Now;
+            Console.WriteLine("Value : {0} , Type : {1} ", MyDynamicVar, MyDynamicVar.GetType());
+            
+            // acts like var but you can change its type
+        }
+    }
+}
